2b1e335552785112dcf769daba89d119248d5691
Vincent Agnano <vincent.agnano@scopyleft.fr>
2015-01-20 15:47:06 -0600

    fixed for dj1.7

--- transadmin/helpers.py
+++ transadmin/helpers.py
@@ -1,5 +1,6 @@
 from django.utils.translation import get_language
 from django.db import DatabaseError
+from django.core.exceptions import AppRegistryNotReady
 
 from jingo import register
 
@@ -12,7 +13,8 @@ def _(text, context=None):
         trans = Translation.objects.translate(text, get_language(), context)
         translated = trans[0].text
         return translated or text
-    except (Translation.DoesNotExist, IndexError, TypeError, DatabaseError):
+    except (Translation.DoesNotExist, IndexError, TypeError, DatabaseError,
+            AppRegistryNotReady):
         return text
 
 
